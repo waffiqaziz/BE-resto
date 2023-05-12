78929867a03eaeb356414239a3476631afbab3ed
arifprbdi19 <123190019@student.upnyk.ac.id>
2023-05-12 23:29:43 +0700

    change db

--- config/database.js
+++ config/database.js
@@ -1,18 +1,17 @@
-import mysql from "mysql2";
+import mysql from 'mysql2';
 
 // create the connection to database
 
 const db = mysql.createConnection({
-    host: "35.225.24.249",
-    user: "dev-arif",
-    password: "password",
-    database: "db_restaurant"
+  host: '35.247.181.16',
+  user: 'dev-ican',
+  password: 'password',
+  database: 'db_restaurant',
 });
 
-
-db.connect(error => {
+db.connect((error) => {
   if (error) throw error;
-  console.log("Successfully connected to the database.");
+  console.log('Successfully connected to the database.');
 });
 
 export default db;
