85d5e2d791b002cc136db50b4761e70fda9ec05c
WaffiqA <waffiq.a2002@gmail.com>
2023-05-22 20:37:59 +0700

    remove socketpath

--- config/database.js
+++ config/database.js
@@ -5,11 +5,11 @@ import mysql from 'mysql2';
 
 const db = mysql.createConnection({
   host: '34.70.96.245',
-  socketpath: 'if-c-01-project:us-central1:tccij5k10t4892m09y0j57w',
   user: 'admin',
   password: 'lkPx9D&k+z4_QOrZ',
   database: 'db_restaurant',
 });
+
 
 db.connect((error) => {
   if (error) throw error;
